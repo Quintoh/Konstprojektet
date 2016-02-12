4e40925f99640d573a0dca580d9a93135e979e8a
Quintoh <anton@strombomarna.se>
2016-02-12 10:11:27 +0100

    Färdig class

--- KonstProjektetV2/KonstProjektetV2/Models/TableModel.cs
+++ KonstProjektetV2/KonstProjektetV2/Models/TableModel.cs
@@ -11,5 +11,16 @@ namespace KonstProjektetV2.Models
 {
     public class TableModel : TableEntity
     {
+        public TableModel(string Artist, string Title)
+        {
+            this.PartitionKey = Artist;
+            this.RowKey = Title;
+        }
+
+        public string Location { get; set; }
+
+        public string Type { get; set; }
+
+        public string Description { get; set; }
     }
 }

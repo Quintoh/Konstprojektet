db1a7e1edd327d358852553f38e4feabfd895f3f
Quintoh <anton@strombomarna.se>
2016-03-09 16:16:16 +0100

    Revert "Ett grund projekt med Lägga till/Ta bort funktioner och koppling till Azure-Table"
    This reverts commit 75863192807338ac647092e79a2d4fd6502bf4a8.

--- KonstProjektetV2/KonstProjektetV2/Controllers/HomeController.cs
+++ KonstProjektetV2/KonstProjektetV2/Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Table;
 using System.Configuration;
+using KonstProjektetV2.Models;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
 
 namespace KonstProjektetV2.Controllers
 {
@@ -14,7 +17,9 @@ namespace KonstProjektetV2.Controllers
     {
         CloudStorageAccount storageAccount;
         CloudTableClient tableClient;
+        CloudBlobClient blobClient;
         CloudTable table;
+        CloudBlobContainer container;
 
         public HomeController()
         {
@@ -24,13 +29,127 @@ namespace KonstProjektetV2.Controllers
             tableClient = storageAccount.CreateCloudTableClient();
 
             table = tableClient.GetTableReference("fouramigos");
+
+            table.CreateIfNotExists();
+
+            blobClient = storageAccount.CreateCloudBlobClient();
+
+            container = blobClient.GetContainerReference("fouramigos");
+
+            container.CreateIfNotExists();
+
+            BlobContainerPermissions permissions = container.GetPermissions();
+            permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+            container.SetPermissions(permissions);
+
+
+            //lägga till nya
+            //var tablemodels = new TableModel("Brutus", "Uggla") { Location = "T4", Description="Uggla i träd", Type="Foto" };
+            //var tablemodels1 = new TableModel("brutus", "Örn") { Location = "T4", Description="Örn som flyger", Type = "Foto" };
+
+            //var opreation = TableOperation.Insert(tablemodels);
+            //var operation2 = TableOperation.Insert(tablemodels1);
+
+            //table.Execute(opreation);
+            //table.Execute(operation2);
         }
 
 
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var query = new TableQuery<TableModel>();
+
+            var tableModels = table.ExecuteQuery(query);
+
+            return View(tableModels);
         }
+
+        //Vissa info om konstverk(ej obligatorisk)
+        public ActionResult Table(string partitionKey, string rowKey)
+        {
+            var operation = TableOperation.Retrieve<TableModel>(partitionKey, rowKey);
+
+            var tablemodel = (TableModel)table.Execute(operation).Result;
+
+
+            var name = tablemodel.Author + "-" + tablemodel.Title + "." + tablemodel.FileEnding;
+
+            var blob = container.GetBlockBlobReference(name);
+
+
+            return View(tablemodel);
+        }
+
+        //lägg till nytt konstverk med bild
+        public ActionResult AddNew()
+        {
+            return View(new TableInsertModel());
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddNew(TableInsertModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+
+            string fileEnding = string.Empty;
+            if(model.File != null)
+            {
+                fileEnding = model.File.FileName.Split('.').Last();
+
+                var name = model.Author + "-" + model.Title + "." + fileEnding;
+
+                var blob = container.GetBlockBlobReference(name);
+
+                await blob.UploadFromStreamAsync(model.File.InputStream);
+            }
+
+            var operation = TableOperation.Insert(new TableModel(model.Author, model.Title) { Description = model.Description, Type = model.Type, Location = model.Location, FileEnding = fileEnding });
+
+            await table.ExecuteAsync(operation);
+
+            return RedirectToAction("Index");
+        }
+
+        //Ta bort konstverk med bild
+        //public ActionResult Delete()
+        //{
+        //    return View();
+        //}
+        //[HttpGet]
+
+        public ActionResult Delete(string partitionKey, string rowKey)
+        {
+            var operation = TableOperation.Retrieve<TableModel>(partitionKey, rowKey);
+
+            var tablemodel = (TableModel)table.Execute(operation).Result;
+
+            var name = tablemodel.Author + "-" + tablemodel.Title + "." + tablemodel.FileEnding;
+            var blob = container.GetBlockBlobReference(name);
+            blob.Delete();
+
+            var list = container.ListBlobs();
+
+            var deleteoperation = TableOperation.Delete(tablemodel);
+
+            table.Execute(deleteoperation);
+
+            return RedirectToAction("Index");
+
+            //return View(tablemodel);
+        }
+
+        //[HttpPost]
+        //public ActionResult Delete(string partitionKey)
+        //{
+        //    var deleteoperation = TableOperation.Delete(tablemodel);
+
+        //    table.Execute(deleteoperation);
+
+        //    return RedirectToAction("Index");
+        //}
     }
 }
--- KonstProjektetV2/KonstProjektetV2/Models/TableModel.cs
+++ KonstProjektetV2/KonstProjektetV2/Models/TableModel.cs
@@ -6,21 +6,61 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Table;
 using System.Configuration;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace KonstProjektetV2.Models
 {
     public class TableModel : TableEntity
     {
-        public TableModel(string Artist, string Title)
+        private string _author;
+        private string _title;
+        public TableModel(string artist, string title)
         {
-            this.PartitionKey = Artist;
-            this.RowKey = Title;
+            Title = title;
+            Author = artist;
         }
+
+        public TableModel() {
+        }
+
+        public string Author { get { return _author; } set { this.PartitionKey = value; _author = value; } }
+
+        public string Title { get { return _title; } set { this.RowKey = value; _title = value; } }
+
 
         public string Location { get; set; }
 
         public string Type { get; set; }
 
         public string Description { get; set; }
+
+        public string FileEnding { get; set; }
+    }
+
+    public class TableInsertModel
+    {
+        [DisplayName("Artist")]
+        [Required]
+        public string Author { get; set; }
+
+        [DisplayName("Titel")]
+        [Required]
+        public string Title { get; set; }
+
+
+        [DisplayName("Plats")]
+        public string Location { get; set; }
+
+        [DisplayName("Typ")]
+        public string Type { get; set; }
+
+        [DisplayName("Beskrivning")]
+        public string Description { get; set; }
+
+        [DisplayName("Bild")]
+        public HttpPostedFileBase File { get; set; }
+
+        public string FileEnding { get; set; }
     }
 }
